6fce8a2aeb809207fd1b229ad9b4071eb81be881
tekpro_s <mined.mob@gmail.com>
2021-02-28 20:03:37 +0900

    second-commit

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -4,10 +4,10 @@
     <div class="card">
       <p>新規登録</p>
       <div class="form">
-        <input placeholder="ユーザーネーム" type="text" />
-        <input placeholder="プロフィール" type="text" />
-        <input placeholder="メールアドレス" type="email" />
-        <input placeholder="パスワード" type="password" />
+        <input placeholder="ユーザーネーム" type="text" v-model="name" />
+        <input placeholder="プロフィール" type="text" v-model="profile" />
+        <input placeholder="メールアドレス" type="email" v-model="email" />
+        <input placeholder="パスワード" type="password" v-model="password" />
         <button @click="auth">新規登録</button>
       </div>
     </div>
