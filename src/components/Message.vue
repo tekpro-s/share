597d601a8ded2d86f4101882b0a464032b989f23
tekpro_s <mined.mob@gmail.com>
2021-03-03 07:33:59 +0900

    second-commit

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -56,7 +56,7 @@ export default {
   // 日付フィルタを設定
   filters: {
     moment: function (date) {
-      return moment(date).format("YYYY/MM/DD HH:mm");
+      return moment(date).format("YYYY/MM/DD HH:mm:ss");
     },
   },
   methods: {
